c1e01a3d86d2e21a246c3fd9af43dccfd85fe31a
Reser, Jack C <jcreser@dmacc.edu>
2020-09-14 23:21:05 -0500

    Initial creation of application for converting age to months.

---
+++ main/camper_age_input.py
@@ -0,0 +1,12 @@
+"""
+
+
+"""
+def convert_to_months(years):
+    months = years * 12
+    return months
+
+if __name__ == '__main__':
+    age_in_years = input("Enter your childs age: ")
+    age_in_months = convert_to_months(int(age_in_years))
+    print("{} years is {} months".format(age_in_years, age_in_months))
